9f73d960fce455b915ec82d9b472a60b8479b7dd
anista13 <2904003na13*>
2023-05-22 23:34:06 +0900

    commit

--- GradProj-main/camera_flask_app.py
+++ GradProj-main/camera_flask_app.py
@@ -18,6 +18,10 @@ global capture, rec_frame, grey, switch, neg, face, rec, out, result, p, capture
 capture = 0
 grey = 0
 switch = 1
+
+captur = 0
+gre = 0
+switc = 1
 global shot_taken
 # make shots directory to save pics
 try:
@@ -26,7 +30,8 @@ except OSError as error:
     pass
 
 
-app = Flask(__name__, template_folder='./templates')#, static_url_path='', static_folder='/static')
+# , static_url_path='', static_folder='/static')
+app = Flask(__name__, template_folder='./templates')
 
 camera = cv2.VideoCapture(0)
 
@@ -38,22 +43,22 @@ def gen_frames():  # generate frame by frame from camera
     while True:
         success, frame = camera.read()
         if success:
-            if (grey):
+            if (grey) or (gre):
                 # make frame green
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-            if (capture):  # capture frame
+            if (capture) or (captur):  # capture frame
                 capture = 0
                 #global img_path
                 now = datetime.datetime.now()
                 p = os.path.sep.join(
                     ['shots', "shot_{}.png".format(str(now).replace(":", ''))])
-                #img_path=p
+                # img_path=p
                 cv2.imwrite(p, frame)
                 global captured_img
-                captured_img=p #save the img as a global variable
-                shot_taken=1
-                ##frame에 사진 찍은거 띄우기 
+                captured_img = p  # save the img as a global variable
+                shot_taken = 1
+                # frame에 사진 찍은거 띄우기
 
             try:
                 ret, buffer = cv2.imencode('.jpg', cv2.flip(frame, 1))
@@ -65,6 +70,7 @@ def gen_frames():  # generate frame by frame from camera
 
         else:
             pass
+
 
 """def take_picture():
     success, frame = camera.read()
@@ -81,10 +87,11 @@ def gen_frames():  # generate frame by frame from camera
     global captured_img
     captured_img=p #save the img as a global variable"""
 
+
 @app.route('/')
 def index():
     global shot_taken
-    shot_taken=0 #사진이 찍힌 상태를 판별할 플래그
+    shot_taken = 0  # 사진이 찍힌 상태를 판별할 플래그
     return render_template('index.html', img="http://127.0.0.1:4000/video_feed")
 
 
@@ -105,32 +112,32 @@ def tasks():
         if request.form.get('click') == 'Capture':
             #global capture
             #capture = 1
-            #shot_taken=1
+            # shot_taken=1
             success, frame = camera.read()
             now = datetime.datetime.now()
             p = os.path.sep.join(
                 ['shots', "shot_{}.png".format(str(now).replace(":", ''))])
-            p=p.replace(" ","-")
+            p = p.replace(" ", "-")
             cv2.imwrite(p, frame)
-            
-            #여기서 글자 탐지, 네모 친 이미지를 static에 저장, return
-            temp_result=extract_infos(p)
+
+            # 여기서 글자 탐지, 네모 친 이미지를 static에 저장, return
+            temp_result = extract_infos(p)
             return render_template('index.html', img="./static/rect.png")
-        
+
         elif request.form.get('grey') == 'Grey':
             global grey
             grey = not grey
         elif request.form.get('start') == 'Start':  # stop or start the camera
-            """if (switch == 1):
+            if (switch == 1):
                 switch = 0
                 camera.release()
                 cv2.destroyAllWindows()
             else:
                 camera = cv2.VideoCapture(0)
-                switch = 1"""
+                switch = 1
             return render_template('index.html', img="http://127.0.0.1:4000/video_feed")
         elif request.form.get('ok') == 'OK':
-            #찍힌 사진이 없는 경우 prompt띄우기
+            # 찍힌 사진이 없는 경우 prompt띄우기
             result = convert_to_text(temp_result)
             print(result)
             return render_template('index.html', email=result[0], title=result[1], text=result[2], img="./static/rect.png")
@@ -151,36 +158,38 @@ def tasks():
         return render_template('index.html', img="http://127.0.0.1:4000/video_feed")
     else:
         return render_template('index.html', img="http://127.0.0.1:4000/video_feed")"""
-    
+
     """#아래 코드 없어도 되는지 확인
     if result is not None:
         return render_template('index.html', email=result[0], title=result[1], text=result[2]) #여기서는 result안줘도 되나?
     else:
         return render_template('index.html')"""
 
+
 @app.route('/uploader', methods=['GET', 'POST'])
 def uploader_file():
-    if request.method=='POST':
-        f=request.files['file']
+    if request.method == 'POST':
+        f = request.files['file']
         #filename= url_for('static', filename='my_image.jpg')
 
-        f.save("./static/"+f.filename)#경로를 static안으로 설정
+        f.save("./static/"+f.filename)  # 경로를 static안으로 설정
         #global result
         #result = convert_to_text(f.filename)
-        #print(result)
+        # print(result)
         #global captured_img
-        #captured_img="./static/"+f.filename
-        
-        #여기서 글자 탐지, 네모 친 이미지를 static에 저장, return
+        # captured_img="./static/"+f.filename
+
+        # 여기서 글자 탐지, 네모 친 이미지를 static에 저장, return
         global temp_result
-        temp_result=extract_infos("./static/"+f.filename)
+        temp_result = extract_infos("./static/"+f.filename)
 
         return render_template('index.html', img="./static/rect.png")
-        #return render_template('index.html', img="http://127.0.0.1:4000/video_feed")
-    
+        # return render_template('index.html', img="http://127.0.0.1:4000/video_feed")
+
+
 @app.route('/send_email', methods=['POST'])
 def send_email():
-    if request.method=='POST':
+    if request.method == 'POST':
         global p
 
         # Get the form data from the request object
@@ -208,7 +217,7 @@ def send_email():
             with open(p, 'rb') as f:
                 img = MIMEImage(f.read())
                 img.add_header('Content-Disposition', 'attachment',
-                            filename=os.path.basename(p))
+                               filename=os.path.basename(p))
                 msg.attach(img)
 
         # Connect to the SMTP server and send the email
@@ -219,8 +228,57 @@ def send_email():
             smtp.sendmail('no-reply@gmail.com', recipient, msg.as_string())
 
         # Return a response to the client
-        #return 'Email sent successfully'
-        return render_template('index.html', img="http://127.0.0.1:4000/video_feed")#성공메세지 출력하기
+        # return 'Email sent successfully'
+        # 성공메세지 출력하기
+        return render_template('index.html', img="http://127.0.0.1:4000/video_feed")
+    
+@app.route('/request', methods=['POST', 'GET'])  # make responsable buttons
+def task():
+
+#     global switc, camer, resul, shot_take, captured_im, img_pat, temp_resul
+#     if request.method == 'POST':
+#         if request.form.get('click') == 'Capture':
+#             success, fram = camer.read()
+#             now = datetime.datetime.now()
+#             p = os.path.sep.join(
+#                 ['shots', "shot_{}.png".format(str(now).replace(":", ''))])
+#             p = p.replace(" ", "-")
+#             cv2.imwrite(p, fram)
+
+#             # 여기서 글자 탐지, 네모 친 이미지를 static에 저장, return
+#             temp_resul = extract_infos(p)
+#             return render_template('en.html', img="./static/rect.png")
+
+#         elif request.form.get('gre') == 'Grey':
+#             global gre
+#             gre = not gre
+#         elif request.form.get('star') == 'Start':  # stop or start the camera
+#             if (switc == 1):
+#                 switc = 0
+#                 camer.release()
+#                 cv2.destroyAllWindows()
+#             else:
+#                 camer = cv2.VideoCapture(0)
+#                 switc = 1
+#             return render_template('en.html', img="http://127.0.0.1:4000/video_feed")
+#         elif request.form.get('ok') == 'OK':
+#             # 찍힌 사진이 없는 경우 prompt띄우기
+#             resul = convert_to_text(temp_resul)
+#             print(resul)
+#             return render_template('en.html', email=resul[0], title=resul[1], text=resul[2], img="./static/rect.png")
+#         """elif request.form.get('upload') == 'Upload': ###여기 다시 보기!!!!!
+#             print("upload pushed")
+#             file = request.files['upload']
+#             file.save(file.filename)
+#             #result = convert_to_text(file)
+#             #print(result)
+#             res=test.gray(file)
+#             print(res)"""
+#     elif request.method == 'GET':
+#         return render_template('en.html')
+#         # return redirect(url_for('tasks'))
+
+    return render_template('en.html', img="http://127.0.0.1:4000/video_feed")
 
 
 if __name__ == '__main__':
@@ -228,37 +286,3 @@ if __name__ == '__main__':
 
 camera.release()
 cv2.destroyAllWindows()
-
-
-@app.route('/request', methods=['POST', 'GET'])  # make responsable buttons
-def test():
-    if request.method == 'POST':
-        if request.form.get('click') == 'Capture':
-            global yes
-            yes = 1
-    return render_template('en.html')
-# def tasks():
-#     global switch, camera, result
-#     if request.method == 'POST':
-#         if request.form.get('click') == 'Capture':
-#             global capture
-#             capture = 1
-#         elif request.form.get('grey') == 'Grey':
-#             global grey
-#             grey = not grey
-#         elif request.form.get('stop') == 'Stop/Start':  # stop or start the camera
-#             if (switch == 1):
-#                 switch = 0
-#                 camera.release()
-#                 cv2.destroyAllWindows()
-#             else:
-#                 camera = cv2.VideoCapture(0)
-#                 switch = 1
-#     elif request.method == 'GET':
-#         return render_template('index.html')
-#         # return redirect(url_for('tasks'))
-
-#     if result is not None:
-#         return render_template('index.html', email=result[0], title=result[1], text=result[2])
-#     else:
-#         return render_template('index.html')
